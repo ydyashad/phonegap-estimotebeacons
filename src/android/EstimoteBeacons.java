0f4e8a5bc916b4c78e3a88d4898c42f127fe575f
anton.s <worker.java@gmail.com>
2013-12-24 11:33:37 +0300

    init

---
+++ src/android/EstimoteBeacons.java
@@ -0,0 +1,145 @@
+package android;
+
+import android.os.RemoteException;
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaInterface;
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: A.Salachyonok
+ * Date: 23.12.13
+ * Class description
+ */
+public class EstimoteBeacons extends CordovaPlugin {
+
+    public static final String START_ESTIMOTE_BEACONS_DISCOVERY_FOR_REGION = "startEstimoteBeaconsDiscoveryForRegion";
+    public static final String STOP_ESTIMOTE_BEACON_DISCOVERY_FOR_REGION = "stopEstimoteBeaconsDiscoveryForRegion";
+
+    public static final String START_RANGING_BEACONS_IN_REGION = "startRangingBeaconsInRegion";
+    public static final String STOP_RANGING_BEACON_IN_REGION = "stopRangingBeaconsInRegion";
+
+    public static final String GET_BEACONS = "getBeacons";
+
+
+    private BeaconManager iBeaconManager = new BeaconManager(this.cordova.getActivity().getApplicationContext());
+
+
+
+    private Region currentRegion;
+
+    private List<Beacon> beacons = new ArrayList<Beacon>();
+
+
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+        currentRegion = new Region("uniqueId", null, null);
+        iBeaconManager.setRangingListener(new BeaconManager.RangingListener() {
+            @Override
+            public void onBeaconsDiscovered(Region region, List<Beacon> beacons) {
+                EstimoteBeacons.this.beacons = beacons;
+            }
+        });
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        try {
+            if (action.equalsIgnoreCase(START_ESTIMOTE_BEACONS_DISCOVERY_FOR_REGION)) {
+                startEstimoteBeaconsDiscoveryForRegion();
+                callbackContext.success(callbackContext.getCallbackId());
+                return true;
+            }
+
+            if (action.equalsIgnoreCase(STOP_ESTIMOTE_BEACON_DISCOVERY_FOR_REGION)) {
+                stopEstimoteBeaconsDiscoveryForRegion();
+                callbackContext.success(callbackContext.getCallbackId());
+                return true;
+            }
+
+            if (action.equalsIgnoreCase(START_RANGING_BEACONS_IN_REGION)) {
+                startRangingBeaconsInRegion();
+                callbackContext.success(callbackContext.getCallbackId());
+                return true;
+            }
+
+            if (action.equalsIgnoreCase(STOP_RANGING_BEACON_IN_REGION)) {
+                stopRangingBeaconsInRegion();
+                callbackContext.success(callbackContext.getCallbackId());
+                return true;
+            }
+
+            if (action.equalsIgnoreCase(GET_BEACONS)) {
+                callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, listToJSONArray(beacons)));
+                return true;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            callbackContext.error(e.getMessage());
+            return false;
+        }
+        return false;
+    }
+
+    private void startEstimoteBeaconsDiscoveryForRegion() throws RemoteException {
+        // TODO: stub
+    }
+
+    private void stopEstimoteBeaconsDiscoveryForRegion() throws RemoteException {
+        // TODO: stub
+    }
+
+    private void startRangingBeaconsInRegion() throws RemoteException{
+        iBeaconManager.startRanging(currentRegion);
+    }
+
+    private void stopRangingBeaconsInRegion() throws RemoteException{
+        iBeaconManager.stopRanging(currentRegion);
+    }
+
+
+
+    /**
+     * Convert list of beacons(@com.estimote.sdk.Beacon) to @JSONArray
+     * @param beacons - list of beacons (@com.estimote.sdk.Beacon)
+     * @return JSONArray
+     * @throws JSONException
+     */
+    private JSONArray listToJSONArray(List<Beacon> beacons) throws JSONException{
+        JSONArray jArray = new JSONArray();
+        for (Beacon beacon : beacons) {
+            jArray.put(beaconToJSONObject(beacon));
+        }
+        return jArray;
+    }
+
+    /**
+     * Convert beacon (@com.estimote.sdk.Beacon) to @JSONObject
+     * @param beacon - beacon(@com.estimote.sdk.Beacon)
+     * @return JSONObject
+     * @throws JSONException
+     */
+    private JSONObject beaconToJSONObject(Beacon beacon) throws JSONException{
+        JSONObject object = new JSONObject();
+        object.put("proximityUUID", beacon.proximityUUID);
+        object.put("major", beacon.major);
+        object.put("minor", beacon.minor);
+        object.put("rssi", beacon.rssi);
+        object.put("macAddress", beacon.macAddress);
+        object.put("measuredPower", beacon.measuredPower);
+        return object;
+    }
+}
